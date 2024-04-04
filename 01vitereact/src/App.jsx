df5566d6442b889d998660b377d3081f19e27fd4
shahnawaz office <shahnawaz.ahmed@zuntechnologys.com>
2024-04-04 14:05:42 +0530

    added vitereact 1st app

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Ready from "./ready"
+function App() {
+  
+
+  return (
+    <>
+    <Ready/>
+    <h1>hello world</h1>
+    <p>testing</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/ready.jsx
@@ -0,0 +1,7 @@
+const Ready=()=>{
+    return(
+        <h1>I'm ready</h1>
+    );
+}
+
+export default Ready
