b67355febec937f0758ec1389806d1039d9fb9aa
shahnawaz office <shahnawaz.ahmed@zuntechnologys.com>
2024-05-06 18:03:11 +0530

    slight update on bgChanger project

--- 04bgcahnger/src/App.jsx
+++ 04bgcahnger/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-  const [bg, setBg] = useState("black");
+  const [bg, setBg] = useState("rgb(236 252 203)");
 
   return (
     <>
@@ -9,20 +9,52 @@ function App() {
         className="w-full h-screen duration-200 ease-in-out"
         style={{ backgroundColor: bg }}
       ></div>
-      <div className="fixed flex-wrap justify-center bottom-10 inset-x-0 px-2">
-        dgadd
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg p-4 bg-white px-2 py-2 rounded-lg">
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-xl">
+      <div className="fixed flex flex-wrap justify-center bottom-14 inset-x-2 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-2 py-2 rounded-2xl">
+          <button onClick={() => setBg("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "red"}}>
             Red
           </button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-xl">
-            Red
+          <button onClick={() => setBg("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "green"}}>
+            Green
           </button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-xl">
-            Red
+          <button  onClick={() => setBg("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "blue"}}>
+            Blue
           </button>
-          <button className="outline-none px-4 py-1 rounded-full text-white shadow-xl">
-            Red
+          <button  onClick={() => setBg("olive")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "olive"}}>
+            Olive
+          </button>
+          <button  onClick={() => setBg("pink")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "pink"}}>
+            Pink
+          </button>
+          <button  onClick={() => setBg("cyan")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "cyan"}}>
+            Cyan
+          </button>
+          <button  onClick={() => setBg("coral")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-xl" 
+          style={{backgroundColor: "coral"}}>
+            Coral
+          </button>
+          <button  onClick={() => setBg("beige")}
+          className="outline-none px-4 py-1 rounded-full shadow-xl" 
+          style={{backgroundColor: "beige"}}>
+            Beige
+          </button>
+          <button  onClick={() => setBg("black")}
+          className="outline-none px-4 py-1 rounded-full shadow-xl text-white" 
+          style={{backgroundColor: "black"}}>
+            Black
           </button>
         </div>
       </div>
